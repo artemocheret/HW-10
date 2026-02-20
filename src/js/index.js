b9cc93737a1bc7703bad4ae911f012217cfb373f
Artem <artemoceret24@gmail.com>
2026-02-20 09:19:46 +0200

    виконав дз

--- src/js/index.js
+++ src/js/index.js
@@ -59,3 +59,25 @@ const isShort = (word) => word.length <= 3;
 console.log("Парних чисел:", countItems(numbers2, isEven));
 console.log("Чисел більше 10:", countItems(numbers2, isLarge));
 console.log("Коротких слів:", countItems(words, isShort));
+
+// 2 завдання
+const calculate = (a, b, operation) => {
+  return operation(a, b);
+};
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => {
+  if (b === 0) {
+    return "Помилка: ділення на нуль!";
+  }
+  return a / b;
+};
+
+console.log(calculate(10, 5, add));
+console.log(calculate(10, 5, subtract));
+console.log(calculate(10, 5, multiply));
+console.log(calculate(10, 5, divide));
+console.log(calculate(10, 0, divide));
